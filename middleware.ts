4eac254c8a9d84904b2954dc84b05448cf325ce6
Mormyshev <mormyshevoleg@gmail.com>
2026-06-11 16:31:10 +0300

    fix: middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,23 @@
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import { updateSession } from "@/src/utils/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
-    return await updateSession(request);
-}
+// export async function middleware(request: NextRequest) {
+//     return await updateSession(request);
+// }
 
-export const config = {
-    matcher: [
-        /*
-         * Перехватываем все маршруты, кроме статики:
-         * - _next/static (статические файлы)
-         * - _next/image (оптимизированные изображения)
-         * - favicon.ico, sitemap.xml, robots.txt
-         * - изображения (svg, png, jpg, jpeg, gif, webp)
-         */
-        "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-    ],
-};
+// export const config = {
+//     matcher: [
+//         /*
+//          * Перехватываем все маршруты, кроме статики:
+//          * - _next/static (статические файлы)
+//          * - _next/image (оптимизированные изображения)
+//          * - favicon.ico, sitemap.xml, robots.txt
+//          * - изображения (svg, png, jpg, jpeg, gif, webp)
+//          */
+//         "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+//     ],
+// };
+export async function middleware(request: NextRequest) {
+  // Временно отключаем проверку сессии для теста
+  return NextResponse.next();
+}
