6b8d0d4c9a0602a5c3eefd7e76c672d2cbeafb75
Mormyshev <mormyshevoleg@gmail.com>
2026-06-11 17:27:47 +0300

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,24 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { type NextRequest } from "next/server";
 import { updateSession } from "@/src/utils/supabase/middleware";
 
-// export async function middleware(request: NextRequest) {
-//     return await updateSession(request);
-// }
-
-// export const config = {
-//     matcher: [
-//         /*
-//          * Перехватываем все маршруты, кроме статики:
-//          * - _next/static (статические файлы)
-//          * - _next/image (оптимизированные изображения)
-//          * - favicon.ico, sitemap.xml, robots.txt
-//          * - изображения (svg, png, jpg, jpeg, gif, webp)
-//          */
-//         "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-//     ],
-// };
 export async function middleware(request: NextRequest) {
-  // Временно отключаем проверку сессии для теста
-  return NextResponse.next();
+  return await updateSession(request);
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Перехватываем все маршруты, КРОМЕ:
+     * - _next/static (статические файлы)
+     * - _next/image (оптимизированные изображения)
+     * - favicon.ico, sitemap.xml, robots.txt
+     * - изображений (svg, png, jpg, jpeg, gif, webp)
+     * - Любых запросов к API и базе данных (исключаем из middleware, чтобы избежать deadlock)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|api|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
+// export async function middleware(request: NextRequest) {
+//   // Временно отключаем проверку сессии для теста
+//   return NextResponse.next();
+// }
