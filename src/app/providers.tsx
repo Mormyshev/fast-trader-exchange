90489f0e0c872cf7e77050c6d18381e3d5e1efc9
Mormyshev <mormyshevoleg@gmail.com>
2026-06-17 00:04:37 +0300

    fix

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,19 @@
 "use client";
-
-import { ThemeProvider } from "next-themes";
 import { AuthProvider } from "./context/AuthContext";
 import SmoothScroll from "../components/SmoothScroll";
 
-export function Providers({ children }: { children: React.ReactNode }) {
-    return (
-        <AuthProvider>
-            <SmoothScroll>{children}</SmoothScroll>
-        </AuthProvider>
-    );
+export function Providers({
+  children,
+  initialUser,
+  initialRole,
+}: {
+  children: React.ReactNode;
+  initialUser?: any;
+  initialRole?: any;
+}) {
+  return (
+    <AuthProvider initialUser={initialUser} initialRole={initialRole}>
+      <SmoothScroll>{children}</SmoothScroll>
+    </AuthProvider>
+  );
 }
