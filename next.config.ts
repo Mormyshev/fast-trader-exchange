28a369d73e021aa61a48f46bc847abb1e5bd8137
Mormyshev <mormyshevoleg@gmail.com>
2026-08-01 11:19:40 +0300

    fix: config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: process.cwd(),
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "bjdmkslrvujlrbqsvgmu.supabase.co",
-        pathname: "/storage/v1/object/public/**",
-      },
-    ],
-  },
+    turbopack: {
+        root: process.cwd(),
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "bjdmkslrvujlrbqsvgmu.supabase.co",
+                pathname: "/storage/v1/object/public/**",
+            },
+        ],
+    },
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["155.212.138.235"], // IP вашего VPS
+        },
+    },
 };
 
 export default nextConfig;
